7225cf52ec662792f25f30ea44f7420f649d1db7
Remuel <Remuel@DESKTOP-B17FN3K>
2025-09-14 18:16:29 +0800

    Add project files.

---
+++ OnlineBookstore.Domain/Order/ValueObjects/BookId.cs
@@ -0,0 +1,24 @@
+﻿using OnlineBookstore.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Domain.Order.ValueObjects
+{
+    public class BookId : ValueObject
+    {
+        public int Value { get; private set; }
+
+        public BookId(int id)
+        {
+            Value = id;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ OnlineBookstore.Domain/Order/ValueObjects/CustomerId.cs
@@ -0,0 +1,24 @@
+﻿using OnlineBookstore.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineBookstore.Domain.Order.ValueObjects
+{
+    public class CustomerId : ValueObject
+    {
+        public int Value { get; private set; }
+
+        public CustomerId(int id)
+        {
+            Value = id;
+        }
+
+        protected override IEnumerable<object> GetEqualityComponents()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
